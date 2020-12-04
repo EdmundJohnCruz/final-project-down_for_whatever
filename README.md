e8f8277ffd2dc5c5531d77fa2341e9b8b4d845ae
Aaron Jacobson <jacobson.s.aaron@gmail.com>
2020-12-03 20:19:26 -0800

    group init work

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Final Project
 
+Final Project for Down with whatever Wowee!
+
 Create a single page react app with microservice back end.
+
+
---
+++ back_end/listingserver.js
@@ -0,0 +1,13 @@
+const express = require('express');
+// import mongodb
+
+const app = express();
+
+
+app.get('/listings', (req, res) => {
+
+});
+
+app.post('/listing', (req, res) => {
+
+});
