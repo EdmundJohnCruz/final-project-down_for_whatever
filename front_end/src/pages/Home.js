c08265c7efd14098d6b5be387e3a32ea0e03fe9b
Wilson Young <wilson_young89@yahoo.com>
2020-12-05 15:51:52 -0800

    Created components folder and populated some of Home.js page

--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -1,1 +1,16 @@
 // Create Homepage here
+import React from 'react';
+
+import ListingCreationForm from '../components/ListingCreationForm';
+import Listings from '../components/Listings';
+
+const Home = () => {
+    return (
+        <div>
+            <hi>Home</hi>
+            <ListingCreationForm />
+            <Inquiries />
+            <Listings />
+        </div>
+    )
+}
