f618037345bb0f6c29737f08e2a42f5cd751ec81
Wilson Young <wilson_young89@yahoo.com>
2020-12-05 17:45:15 -0800

    Progress on Home.js

--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -9,27 +9,26 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
-//import Container from '/react-bootstrap/Container';
 import 'bootstrap/dist/css//bootstrap.min.css'; //imports css needed for bootstrap to work
 
 const Home = () => {
     return (
         <div>
             <Navbar bg="light" expand="lg" sticky="top">
-                <Navbar.Brand href="#home">Ghetto eBay</Navbar.Brand>
+                <Navbar.Brand>Ghetto eBay</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <NavDropdown title="Create Post" id="basic-nav-dropdown">
                         </NavDropdown>
                         <Form inline>
-                                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                                <Button variant="outline-success">Search</Button>
-                            </Form>
+                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
                         <NavDropdown title="Log In" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Log In</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/signup">Need An Account?</NavDropdown.Item>   
+                            <NavDropdown.Item>Log In</NavDropdown.Item>
+                            
+                            <NavDropdown.Item href="/signup">Need An Account?</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
