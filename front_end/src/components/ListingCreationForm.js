2cfafdd33ef1421770ca4d9f2eae69a3ea8e00c5
LamGordon <lamgordon1998@gmail.com>
2020-12-08 06:09:48 -0800

    redid listing creation form

--- front_end/src/components/ListingCreationForm.js
+++ front_end/src/components/ListingCreationForm.js
@@ -1,53 +1,67 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CurrencyInput from 'react-currency-input-field'
+import Form from 'react-bootstrap/Form';
+import { FormControl, InputGroup } from 'react-bootstrap';
 const axios = require('axios');
 
 const url = "http://locahost:4001";
 
 class ListingCreationForm extends Component {
-    state = {
-        title: "",
-        description: "",
-        price: "",
-    };
-    //  missing userid and images for now (should timestamp be handled in backend?)
-
-    onTitleChange = e => {
-        this.setState({
-            title: e.target.value
-        });
-    };
-
-    onDescriptionChange = e => {
-        this.setState({
-            description: e.target.value
-        });
-    };
-
-    onPriceChange = (value) => {
-        this.setState({
-            price: value
-        });
-    };
 
     handleSubmit = e => {
         e.preventDefault();
         const data = {
             userid: 'placeholder',
-            title: this.state.title,
-            description: this.state.description,
-            price: this.state.price,
-            userid: 'placeholder',
+            title: document.getElementById("title").value,
+            description: document.getElementById("description").value,
+            price: document.getElementById("price").value,
         }
         console.log(`\n\n~~~~~~~~~~~~~~~~~~~~\n\n Data : ${JSON.stringify(data)} \n\n~~~~~~~~~~~~~~~~~~~~\n\n`);
-        axios.post(url+"/listings", {listing: data})
+        axios.post(url + "/listings", { listing: data })
             .then(res => console.log(res))
             .catch(err => console.log(err));
     };
-    
+
     render() {
         return (
             <div className="ListingCreationForm">
+
+                <Form onSubmit={this.handleSubmit}>
+                    <h1>Create a New Listing</h1>
+                    <Form.Group>
+                        <Form.Label class="font-weight-bold " >Title</Form.Label>
+                        <Form.Control id="title" type="title" placeholder="Enter the Title of the Listing" required />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label class="font-weight-bold">Description</Form.Label>
+                        <Form.Control id="description" type="description" as="textarea" rows={4} placeholder="Enter a Description : &#13;&#10; Hello" required />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label class="font-weight-bold">Price</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Prepend>
+                                <InputGroup.Text>$</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl id="price" type="number" placeholder="0.00" min="0" step=".01" pattern="^\d+(?:\.\d{1,2})?$" required />
+                        </InputGroup>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label class="font-weight-bold">Please Provide an Image for the Listing</Form.Label>
+                        <Form.File type="image" label="Upload your image here" custom />
+                    </Form.Group>
+                    
+                    <button type="submit" class="btn btn-primary" >Create Listing</button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+/*
+
                 <form onSubmit={this.handleSubmit}>
                     <h1>Create a New Listing</h1>
                     <input placeholder="Title" value={this.state.title} onChange={this.onTitleChange} required />
@@ -55,9 +69,6 @@ class ListingCreationForm extends Component {
                     <CurrencyInput placeholder="$0.00" allowDecimals={true} prefix={'$'} precision={2} allowNegativeValue={false} onChange={(value) => this.onPriceChange(value)} required/>
                     <button type="submit">Create Listing</button>
                 </form>
-            </div>
-        )
-    }
-}
+*/
 
 export default ListingCreationForm;
