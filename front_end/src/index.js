2c9ba92fa2fb3e14bd9b89e09e3f9df8693cfe8b
EdmundJohnCruz <edmundjohncruz@gmail.com>
2020-12-12 14:52:42 -0800

    state is saved throughout website after logging in

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -8,10 +8,43 @@ import thunk from 'redux-thunk';
 import App from './App.js';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const loadState = () => {
+  try {
+    const targetState = localStorage.getItem('state');
+    if(targetState === null){
+      return undefined;
+    }
+    return JSON.parse(targetState);
+  }
+  catch (err) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const targetState = JSON.stringify(state);
+    localStorage.setItem('state',targetState);
+  }
+  catch (err){
+    return console.log('there was an error with saveState');
+  }
+};
+
+const loginState = loadState();
+
+const store = createStore(rootReducer, loginState, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  console.log('store.getState(): ', store.getState());
+  console.log('store.getState()userReducer: ', store.getState().userReducer);
+
+  saveState({
+    userReducer: store.getState().userReducer
+  });
+});
 
 ReactDOM.render(
   <Provider store={store}>
