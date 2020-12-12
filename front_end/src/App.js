f516b816e3405820ec46ebf61c5208417962fd0c
Aaron Jacobson <jacobson.s.aaron@gmail.com>
2020-12-12 13:19:41 -0800

    add websocket creation to App.js

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -4,9 +4,22 @@ import Home from './pages/Home.js';
 import SignUp from './pages/SignUp.js';
 //  testing
 import ListingCreationForm from './components/ListingCreationForm'
-//const webSocket = new webSocket('ws://localhost:3000');
 
 const App = () => {
+    const ws = new WebSocket('ws://' + window.location.host.split(':')[0] + (window.location.port && `:${window.location.port}`) + '/websocket');
+    ws.onopen = (ev) => {
+        console.log('opened ws');
+    };
+    ws.onclose = (ev) => {
+        console.log('closed ws');
+    };
+    ws.onerror = (ev) => {
+        console.log('ws error: ', ev);
+    };
+    ws.onmessage = (ev) => {
+        console.log('ws message: ', ev);
+    };
+
     return (
         <Switch>
             <Route path="/home">
@@ -19,7 +32,6 @@ const App = () => {
                 <ListingCreationForm />
             </Route>
         </Switch>
-
     );
 }
 
