5d41a9f1f867f6c3444baf8fe49bb901f010004a
Aaron Jacobson <jacobson.s.aaron@gmail.com>
2020-12-12 15:16:03 -0800

    add ws new listing refresh code

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -8,11 +8,26 @@ import thunk from 'redux-thunk';
 import App from './App.js';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
+import {setListings} from './redux/actions/listingActions';
 
 import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
+// ws update helpers
+const getListings = (dispatch) => { // also called in Listings.js
+  axios.get('/api/listingserver/listings')
+  .then( (res) => {
+    console.log('updating listings: ', res.data);
+      dispatch(setListings(res.data.listings));
+  })
+  .catch( (err) => {
+    console.log('update listings failed in index.js');
+    console.log(err);
+  })
+};
+
+// set ws actions for different messages
 const ws = new WebSocket('ws://' + window.location.host.split(':')[0] + (window.location.port && `:${window.location.port}`) + '/websocket');
 ws.onopen = (ev) => {
   console.log('opened ws');
@@ -25,6 +40,11 @@ ws.onerror = (ev) => {
 };
 ws.onmessage = (ev) => {
   console.log('ws message: ', ev);
+  const data = JSON.parse(ev.data);
+  if(data.message === 'listingChange'){
+    console.log('updating listings...');
+    getListings(store.dispatch);
+  }
 };
 
 
