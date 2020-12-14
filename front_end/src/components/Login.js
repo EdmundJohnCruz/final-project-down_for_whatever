c17ceb9c12ddfc753f2476fbe94babf7ed38162c
EdmundJohnCruz <edmundjohncruz@gmail.com>
2020-12-14 03:44:02 -0800

    changed axios a bit, excluding bcrypt and changed userId to username

--- front_end/src/components/Login.js
+++ front_end/src/components/Login.js
@@ -4,39 +4,37 @@ import Form from 'react-bootstrap/Form';
 import { FormControl, InputGroup, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { setUserName, setIsLoggedIn } from '../redux/actions/userActions';
-//import bcrypt from 'bcrypt';
 
 
 const Login = () => {
-    const [userId, setUserId] = React.useState();
-    const [typedPassword, setTypedPassword] = React.useState(); //state based on user's inputted password
+    const [username, setUsername] = React.useState();
+    const [password, setPassword] = React.useState(); //state based on user's inputted password
+    const [error, setError] = React.useState();
     const dispatch = useDispatch();
 
     const handleSubmit = e => {
         e.preventDefault();
 
-        /*axios.post('/api/login',{ //grabbing userId and password from DB
-            userId: '',
+        /*axios.post('/api/login', { //grabbing username and password from DB
+            username: '',
             password: '',
         })
-         .then((res) => {
-             console.log(res.data);
-             if(res.data.success){
-                bcrypt.compare(typedPassword, password, function(err,res) { //comparing inputted password with hashed password in DB(?)
-                    if (res == true){
-                        console.log("Passwords match. User can login.");
-                        dispatch(setUserName(userId));
-                        dispatch(setIsLoggedIn(true));
-                    }
-                    else{
-                        console.log(err);
-                    }
-                 });
-                console.log("User logged in!");
-            }
-         });*/
+            .then((res) => {
+                console.log(res.data);
+                if (res.data.success) {
+                    console.log("User successfully logged in.");
+                    dispatch(setUserName(username));
+                    dispatch(setIsLoggedIn(true));
+                }
+                else {
+                    setError(res.data.error);
+                }
+            })
+            .catch(() => {
+                setError("An error occured while attempting to login.");
+            });*/
 
-        dispatch(setUserName(userId));
+        dispatch(setUserName(username));
         dispatch(setIsLoggedIn(true));
     };
 
@@ -45,12 +43,12 @@ const Login = () => {
             <Form onSubmit={handleSubmit} className="text-center" style={{ padding: "10px" }} >
                 <Form.Group>
                     <Form.Label class="font-weight-bold">Username :</Form.Label>
-                    <Form.Control id="username" type="username" placeholder="Username" onChange={(e) => setUserId(e.target.value)} required />
+                    <Form.Control id="username" type="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} required />
                 </Form.Group>
 
                 <Form.Group>
                     <Form.Label class="font-weight-bold">Password :</Form.Label>
-                    <Form.Control id="username" type="username" placeholder="Password" required onChange={(e) =>setTypedPassword(e.target.value)} />
+                    <Form.Control id="username" type="username" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} />
                 </Form.Group>
                 <Button type="submit" className="btn-primary float-center">Log In</Button>
             </Form>
