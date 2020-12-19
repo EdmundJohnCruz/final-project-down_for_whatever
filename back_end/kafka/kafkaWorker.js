ae8f41bb28c8b6fd8de12b6b6787395bd1ffcae4
rachit-joshi <rachit.joshi.apk1@gmail.com>
2020-12-19 09:29:03 -0800

    kafka test remaining v2

--- back_end/kafka/kafkaWorker.js
+++ back_end/kafka/kafkaWorker.js
@@ -4,87 +4,87 @@ const {MongoClient, ObjectId} = require('mongodb');
 
 const consumer = new KafkaConsumer(['imgresize']);
 
-// const auth = 'dfw:dfw123';
-// const dbName = '667Final';
-// const url = `mongodb+srv://${auth}@cluster0.gefuv.mongodb.net/?retryWrites=true&w=majority`;
-// const listingCollectionName = 'Listings';
+const auth = 'dfw:dfw123';
+const dbName = '667Final';
+const url = `mongodb+srv://${auth}@cluster0.gefuv.mongodb.net/?retryWrites=true&w=majority`;
+const listingCollectionName = 'Listings';
 
-// const dbClient = new MongoClient(url);
+const dbClient = new MongoClient(url);
 
-// consumer.on('message', (message) => {
-//     console.log('message recieved on kafkaworker');
-//     console.log(message);
+ consumer.on('message', (message) => {
+     console.log('message recieved on kafkaworker');
+     console.log(message);
 
-//     dbClient.connect((error) => {
-//         if(error) {
-//           console.log('error! can\'t connect to DB instance');
-//           console.log(error);
-//           process.exit(1);
-//         }
+    dbClient.connect((error) => {
+        if(error) {
+          console.log('error! can\'t connect to DB instance');
+          console.log(error);
+          process.exit(1);
+        }
       
-//         console.log("Connected!");
+        console.log("Connected!");
       
-//         const db = dbClient.db(dbName);
-//         const listingCollection = db.collection(listingCollectionName);
-//         img_id = new ObjectId("5fdd12b07cd5322a93062a92");
-//         let query = { "_id": img_id };
-//         listingCollection.findOne(query)
-//           .then((doc) => {
-//               // console.log(doc._id);
-//               sharp(doc.og_img.data.buffer).resize(100, 100, {
-//                   fit: sharp.fit.inside,
-//                   withoutEnlargement: true
-//               })
-//               .toFormat('jpeg')
-//               .toBuffer()
-//               .then(function(outputBuffer){
-//                 //  console.log(outputBuffer);
-//                 const listingIdToEdit = ObjectId(doc._id);
-//                 const filter = {_id: listingIdToEdit};
-//                 listingCollection.updateOne(filter, {$addToSet : {
-//                     smaller_img: {
-//                         data: outputBuffer,
-//                         contentType: doc.og_img.contentType,
-//                     }
-//                 }}, (err, dbRes) => {
-//                     if(err) {
-//                         console.log(`error! can\'t edit ${listingIdToEdit}`);
-//                         console.log(err);
-//                       } else {
-//                       console.log('edited listingID: ', listingIdToEdit);
-//                       }
-//                     });
-//               });
-//               sharp(doc.og_img.data.buffer).resize(500, 500, {
-//                         fit: sharp.fit.inside,
-//                         withoutEnlargement: true,
-//                     })
-//                     .toFormat('jpeg')
-//                     .toBuffer()
-//                     .then(function(outputBuffer){
-//                       // console.log(outputBuffer);
-//                       const listingIdToEdit = ObjectId(doc._id);
-//                       const filter = {_id: listingIdToEdit};
-//                       listingCollection.updateOne(filter, {$addToSet : {
-//                           small_img: {
-//                               data: outputBuffer,
-//                               contentType: doc.og_img.contentType,
-//                           }
-//                       }}, (err, dbRes) => {
-//                           if(err) {
-//                               console.log(`error! can\'t edit ${listingIdToEdit}`);
-//                               console.log(err);
-//                             } else {
-//                             console.log('edited listingID: ', listingIdToEdit);
-//                             }
-//                           });
-//                     });
-//           })
-//           .catch((e) => {
-//               console.log("error: ", e);
-//           });
-//       });
-//       dbClient.close();
+        const db = dbClient.db(dbName);
+        const listingCollection = db.collection(listingCollectionName);
+        img_id = new ObjectId(message);
+        let query = { "_id": img_id };
+        listingCollection.findOne(query)
+          .then((doc) => {
+              // console.log(doc._id);
+              sharp(doc.og_img.data.buffer).resize(100, 100, {
+                  fit: sharp.fit.inside,
+                  withoutEnlargement: true
+              })
+              .toFormat('jpeg')
+              .toBuffer()
+              .then(function(outputBuffer){
+                //  console.log(outputBuffer);
+                const listingIdToEdit = ObjectId(doc._id);
+                const filter = {_id: listingIdToEdit};
+                listingCollection.updateOne(filter, {$addToSet : {
+                    smaller_img: {
+                        data: outputBuffer,
+                        contentType: doc.og_img.contentType,
+                    }
+                }}, (err, dbRes) => {
+                    if(err) {
+                        console.log(`error! can\'t edit ${listingIdToEdit}`);
+                        console.log(err);
+                      } else {
+                      console.log('edited listingID: ', listingIdToEdit);
+                      }
+                    });
+              });
+              sharp(doc.og_img.data.buffer).resize(500, 500, {
+                        fit: sharp.fit.inside,
+                        withoutEnlargement: true,
+                    })
+                    .toFormat('jpeg')
+                    .toBuffer()
+                    .then(function(outputBuffer){
+                      // console.log(outputBuffer);
+                      const listingIdToEdit = ObjectId(doc._id);
+                      const filter = {_id: listingIdToEdit};
+                      listingCollection.updateOne(filter, {$addToSet : {
+                          small_img: {
+                              data: outputBuffer,
+                              contentType: doc.og_img.contentType,
+                          }
+                      }}, (err, dbRes) => {
+                          if(err) {
+                              console.log(`error! can\'t edit ${listingIdToEdit}`);
+                              console.log(err);
+                            } else {
+                            console.log('edited listingID: ', listingIdToEdit);
+                            }
+                          });
+                    });
+          })
+          .catch((e) => {
+              console.log("error: ", e);
+          });
+      });
+      dbClient.close();
 });
 
 consumer.connect();
